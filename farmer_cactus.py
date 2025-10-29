d584a5fd5d39b904a48ff5322dd9a447fa1d98f0
GibsDev <notcurtisg@gmail.com>
2025-10-29 02:23:58 -0400

    cactus farmer checkpoint

--- farmer_cactus.py
+++ farmer_cactus.py
@@ -2,7 +2,7 @@ import u
 import plots
 import algos
 
-CACTUS_LINE_SIZE = get_world_size()
+CACTUS_LINE_SIZE = 7
 
 # Cactus takes 1 second to grow
 
@@ -46,42 +46,107 @@ def run():
 		size = measure()
 		algos.binary_insert(sorted_state, size, number_comp)
 		state.append(size)
+		
+	quick_print("initial state:", state)
+	quick_print("target state:", sorted_state)
+	quick_print("")
 	
-	quick_print(state)
-	quick_print(sorted_state)
+	# TODO implement algo
 
-	# Move the cactuses around to meet the spec
-	min_index = 0
-	max_index = CACTUS_LINE_SIZE - 1
+	index_lookup = {}
+	# Insert empty arrays for all possible cactus values
+	for i in range(10):
+		index_lookup[i] = []
+	# Build the lookup
+	for i in range(len(sorted_state)):
+		algos.binary_insert(index_lookup[sorted_state[i]], i, number_comp)
 
-	u.go_to_plot((0, 0))
-	while min_index < max_index:
-		while measure() == sorted_state[get_pos_x()] and get_pos_x() < max_index:
-			move(East)
-			min_index = min_index + 1
-		# Scan through and grab max
-		for i in range(max_index - min_index):
-			if measure() == sorted_state[max_index]:
-				# "grab" the size we need for the end
-				swap(East)
+	# index_lookup should now look like:
+	# {1: [0, 1, 2], 2: [3], 3: [4, 5], 4: [5]}
+	# For sorted_state: [1, 1, 1, 2, 3, 3, 4]
+
+	quick_print("index_lookup", index_lookup)
+
+	# use the sorted list to calculate the deltas for the slots
+	deltas = []
+	for i in range(len(state)):
+		value = state[i]
+		indexes_for_current_value = index_lookup[value]
+		min_index = indexes_for_current_value[0]
+		max_index = indexes_for_current_value[-1]
+		delta = 0
+		if i >= min_index and i <= max_index:
+			delta = 0
+		else:
+			# Find the smallest delta max vs min
+			min_delta = min_index - i
+			max_delta = max_index - i
+			if abs(min_delta) < abs(max_delta):
+				delta = min_delta
 			else:
-				do_sort()
-			move(East)
-		max_index = max_index - 1
-		move(West)
-		while measure() == sorted_state[get_pos_x()] and get_pos_x() > min_index:
-			move(West)
-			max_index = max_index - 1
-		# Scan through and grab min
-		for i in range(max_index - min_index):
-			if measure() == sorted_state[min_index]:
-				# "grab" the size we need for the end
-				swap(West)
-			else:
-				do_sort()
-			move(West)
-		min_index = min_index + 1
-		move(East)
+				delta = max_delta
+		deltas.append(delta)
+
+	quick_print("deltas", deltas)
+
+	# TODO virtually simulate the swapping algorithm
+	# and record steps so we can play them back
+
+	current_index = 9
+
+	# TODO repeat swapping algorithm until all deltas are 0
+	# how can we efficiently check that? Keep a running tally?
+
+	# helper function for performing swaps by keeping track of state
+	def do_swap():
+		# TODO perform swaps and keep track of state
+
+	# Get the nearest delta that is not 0 and perform the swap
+	# swap in the direction of the delta value
+	# increment or decrement any non 0 passed values accordingly
+	# keep an eye out for running into finished (0 delta) cactuses of the same size and stop early
+
+
+
+
+
+
+
+	# # Move the cactuses around to meet the spec
+	# min_index = 0
+	# max_index = CACTUS_LINE_SIZE - 1
+
+	# u.go_to_plot((0, 0))
+	# while min_index < max_index:
+	# 	while measure() == sorted_state[get_pos_x()] and get_pos_x() < max_index:
+	# 		move(East)
+	# 		min_index = min_index + 1
+	# 	# Scan through and grab max
+	# 	for i in range(max_index - min_index):
+	# 		if measure() == sorted_state[max_index]:
+	# 			# "grab" the size we need for the end
+	# 			swap(East)
+	# 		else:
+	# 			do_sort()
+	# 		move(East)
+	# 	max_index = max_index - 1
+	# 	move(West)
+	# 	while measure() == sorted_state[get_pos_x()] and get_pos_x() > min_index:
+	# 		move(West)
+	# 		max_index = max_index - 1
+	# 	# Scan through and grab min
+	# 	for i in range(max_index - min_index):
+	# 		if measure() == sorted_state[min_index]:
+	# 			# "grab" the size we need for the end
+	# 			swap(West)
+	# 		else:
+	# 			do_sort()
+	# 		move(West)
+	# 	min_index = min_index + 1
+	# 	move(East)
+
+	while True:
+		do_a_flip()
 
 	harvest()
 
